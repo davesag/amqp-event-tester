67df774645e01626a79d01993757d0830950b4ac
Dave Sag <davesag@gmail.com>
2020-04-10 13:30:10 +1000

    n/a updated prettier and prettier configs

--- src/utils/Cache.js
+++ src/utils/Cache.js
@@ -2,21 +2,21 @@ function Cache() {
   this.database = {}
 }
 
-Cache.prototype.get = function(key) {
+Cache.prototype.get = function (key) {
   return this.database[key]
 }
 
-Cache.prototype.set = function(key, value) {
+Cache.prototype.set = function (key, value) {
   this.database[key] = value
 }
-Cache.prototype.clear = function(key) {
+Cache.prototype.clear = function (key) {
   delete this.database[key]
 }
-Cache.prototype.keys = function() {
+Cache.prototype.keys = function () {
   return Object.keys(this.database)
 }
 
-Cache.prototype.reset = function() {
+Cache.prototype.reset = function () {
   this.database = {}
 }
 
